a39bae0ff83ea1a09c9fcdd5d923a67e99b7a636
rotem <ksdjfnlisdfidsul>
2022-05-17 21:10:05 +0300

    changed file-saving strategy, added vocabulary options, cosine sim, hyperparam definition in ClusterSender, word2vec, interactive api, choosing among optimal clues in ExhaustiveSender

--- rotem_main.py
+++ rotem_main.py
@@ -14,9 +14,11 @@ import time
 
 
 class EmbeddingClusterSender(rotem_utils.EmbeddingAgent):
-    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str):
-        super(EmbeddingClusterSender, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
-        self.cluster_amounts = [self.tca // (i + 1) for i in range(4)]
+    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str, dist_metric: str):
+        super(EmbeddingClusterSender, self).__init__(total_cards, good_cards, embedding_method, vocab_method, dist_metric)
+        cluster_range = 4
+        multiplicity = 4
+        self.cluster_amounts = sum([[self.tca // (i + 1)]*multiplicity for i in range(cluster_range)], start=[])
         self.good_embeds = None
         self.bad_embeds = None
 
@@ -62,15 +64,21 @@ class EmbeddingClusterSender(rotem_utils.EmbeddingAgent):
         centroid, indices, clue_len = self.largest_good_cluster()
         clue_word = self.find_closest_word(centroid, good_words + bad_words, verbose=verbose)
         if verbose:
-            chosen_words = [good_words[i] for i in indices]
-            print(f"for {clue_len} chosen words: \n \t {chosen_words} \n the clue is {clue_word}")
+            targets = sorted([good_words[i] for i in indices])
+        else:
+            targets = None
 
-        return clue_word, clue_len
+        return clue_word, clue_len, targets
 
 
 class ExhaustiveSearchSender(rotem_utils.EmbeddingAgent):
-    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str):
-        super(ExhaustiveSearchSender, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
+    """
+    A sender agent which operates as follows - given good and bad cards, finds the set of all words in the vocabulary
+    with maximal amount of good cards closer than all bad cards. Out of this set, the agent chosses the word which is
+    closest to it's matching good cards.
+    """
+    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str, dist_metric: str):
+        super(ExhaustiveSearchSender, self).__init__(total_cards, good_cards, embedding_method, vocab_method, dist_metric)
 
     def forward(self, words: tuple, verbose: bool = False):
         """
@@ -89,21 +97,34 @@ class ExhaustiveSearchSender(rotem_utils.EmbeddingAgent):
         boolean_closeness_mat = (good_diff < closest_bad_distance)
         close_good_cards = boolean_closeness_mat.sum(dim=1)  # amount of good cards that are closer than the closest
         # bad card - shape (N_vocab)
-        close_good_cards[[self.word_index_dict[word] for word in good_words + bad_words]] = 0   # to avoid
+        close_good_cards[[self.word_index_dict[word] for word in good_words + bad_words]] = 0   # avoid current cards
 
-        best_word_cluesize, best_word_idx = torch.max(close_good_cards, 0)
+        # new - choose the best option among the optimal clues ####################
+        all_best_indices = torch.nonzero(close_good_cards == close_good_cards.max()).view(-1)
+        best_word_cluesize = close_good_cards[all_best_indices[0]]
+        print("amount of optimal clues:", len(all_best_indices))
+        best_good_diff = good_diff[all_best_indices]
+        closest_bad_distance = closest_bad_distance[all_best_indices]
+        best_good_diff = torch.where(best_good_diff < closest_bad_distance,
+                                     best_good_diff,
+                                     torch.zeros(best_good_diff.shape, dtype=torch.float)).sum(dim=1)
+        best_word_idx = torch.argmin(best_good_diff)        # index of best word among optimals
+        best_word_idx = all_best_indices[best_word_idx]     # that word's index in the entire vocabulary
+        ######################################################
+
+        # best_word_cluesize, best_word_idx = torch.max(close_good_cards, 0)
         clue_word = self.index_word_dict[best_word_idx.item()]
         if verbose:
             cluster_words_indices = boolean_closeness_mat[best_word_idx].nonzero(as_tuple=True)[0]
-            # print(f"For the {best_word_cluesize.item()} "
-            #       f"good word: \n \t {[good_words[i] for i in cluster_words_indices]}\n \t The chosen clue is {clue_word}")
-            print(f"target words: {sorted([good_words[i] for i in cluster_words_indices])} \n \t guess: {clue_word}")
-        return clue_word, best_word_cluesize.item()
+            targets = sorted([good_words[i] for i in cluster_words_indices])
+        else:
+            targets = None
+        return clue_word, best_word_cluesize.item(), targets
 
 
 class EmbeddingNearestReceiver(rotem_utils.EmbeddingAgent):
-    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str):
-        super(EmbeddingNearestReceiver, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
+    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str, dist_metric: str):
+        super(EmbeddingNearestReceiver, self).__init__(total_cards, good_cards, embedding_method, vocab_method, dist_metric)
 
     def forward(self, words: List[str], clue: tuple):
         """
@@ -118,12 +139,12 @@ class EmbeddingNearestReceiver(rotem_utils.EmbeddingAgent):
 
 
 class CodenameDataLoader:
-    def __init__(self, total_cards_amount: int, good_cards_amount: int, embed_method: str = 'GloVe',
-                 vocab_method: str = 'wordnet-nouns'):
+    def __init__(self, total_cards_amount: int, good_cards_amount: int, embed_method1: str = 'GloVe',
+                 embed_method2: str = 'GloVe', vocab_method: str = 'wordnet-nouns'):
         """
         Note: the embed_method is only used to filter the vocabulary in this class.
         """
-        *_, self.vocab = rotem_utils.get_embedder_with_vocab(embed_method, vocab_method)
+        self.vocab = rotem_utils.triple_restricted_vocab(embed_method1, embed_method2, vocab_method)
         self.vocab_length = len(self.vocab)
         self.random_mask = torch.zeros(self.vocab_length)
         self.random_mask[:1000] = 1
@@ -142,81 +163,120 @@ class CodenameDataLoader:
             receiver_input = random.sample(words, len(words))   # shuffled words
             yield sender_input, receiver_input
 
-
-class TrainableSender(rotem_utils.EmbeddingAgent):
-    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str):
-        super(TrainableSender, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
-        self.vocab_size = len(self.word_index_dict)
-        self.fc = nn.Linear(100, self.vocab_size)
-
-    def forward(self, x, aux_input=None):
-        print(f"sender inputs: \n \t x: {x} \n \t aux: {aux_input} \n \n")
-        return None
+    def legal_word(self, word):
+        return word in self.vocab
 
 
-class TrainableReceiver(rotem_utils.EmbeddingAgent):
-    def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str, input_size: int):
-        super(TrainableReceiver, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
-        self.fc = nn.Linear(input_size, 10)     # 10 tbd
+def skyline_main(tca, gca, sender_type, sender_emb_method, receiver_emb_method, vocab_method, dist_metric,
+                 trial_amount, verbose):
+    """
 
-    def forward(self, channel_input, receiver_input, aux_input=None):
-        print(f"Receiver inputs: \n \t channel_input: {channel_input} \n \t receiver_input: {receiver_input}"
-              f" \n \t aux: {aux_input} \n \n")
-        return None
+    Parameters
+    ----------
+    tca - total cards amount
+    gca - good cards amount
+    sender_type - 'exhaustive' or 'cluster', which sender class to use
+    sender_emb_method - 'GloVe' or 'word2vec'
+    receiver_emb_method - 'GloVe' or 'word2vec'
+    vocab_method - 'GloVe' or 'word2vec' or 'nltk-words' or 'wordnet-nouns' or 'nouns-kaggle'
+    dist_metric - 'l2' or 'cosine_sim'
+    trial_amount - how many games to run for the evaluation
+    verbose - bool
 
+    Returns
+    -------
 
-def skyline_main():
-    gca = 10
-    tca = 20
-    embedding_method = 'GloVe'
-    vocab_method = 'wordnet-nouns'
-    kwargs = dict(total_cards=tca, good_cards=gca, embedding_method=embedding_method, vocab_method=vocab_method)
-    trial_amount = 6
+    """
+    kwargs = dict(total_cards=tca, good_cards=gca, vocab_method=vocab_method, dist_metric=dist_metric)
 
-    sender = ExhaustiveSearchSender(**kwargs)
-    # sender = EmbeddingClusterSender(**kwargs)
-    receiver = EmbeddingNearestReceiver(**kwargs)
-    dataloader = CodenameDataLoader(tca, gca, embedding_method, vocab_method)
+    sender_class = {'exhaustive': ExhaustiveSearchSender, 'cluster': EmbeddingClusterSender}[sender_type]
+    sender = sender_class(**kwargs, embedding_method=sender_emb_method)
+    receiver = EmbeddingNearestReceiver(**kwargs, embedding_method=receiver_emb_method)
+    dataloader = CodenameDataLoader(tca, gca, sender_emb_method, receiver_emb_method, vocab_method)
     i=0
     for sender_input, receiver_input in dataloader:
         # print(f"receiver_input - all words: {receiver_input}\n")
         # print(f"sender input:\n \tgood words: \t {sender_input[0]}\n \tbad words: \t {sender_input[1]}")
-        print(f"\tgood words: \t {sender_input[0]}\n \tbad words: \t {sender_input[1]}")
-        t0 = time.perf_counter()
-        clue = sender(sender_input, verbose=True)
-        print("sender time:", time.perf_counter() - t0)
+        # print(f"\tgood words: \t {sender_input[0]}\n \tbad words: \t {sender_input[1]}")
+        # t0 = time.perf_counter()
+        *clue, targets = sender(sender_input, verbose=verbose)
+        # print(f"target words: {targets} \n \t clue: {clue[0]} \n")
+        # print("sender time:", time.perf_counter() - t0)
         choice = receiver(receiver_input, clue)
-        print("guess:", sorted(choice), "\n\n")
+        print(f"clue size {clue[1]}, guess success: {100 * len([x for x in choice if x in targets]) / len(targets)}%")
+        # print("guess:", sorted(choice), "\n\n")
         i += 1
         if i == trial_amount:
             break
 
 
-def main():
-    gca = 4
-    tca = 10
-    embedding_method = 'GloVe'
-    vocab_method = 'wordnet-nouns'
-    rec_input_size = 400
-    kwargs = dict(total_cards=tca, good_cards=gca, embedding_method=embedding_method, vocab_method=vocab_method)
-    opts = core.init(params=['--random_seed=7',  # will initialize numpy, torch, and python RNGs
-                             '--lr=1e-3',  # sets the learning rate for the selected optimizer
-                             '--batch_size=32',
-                             '--optimizer=adam'])
+def interactive_game():
+    print("This is an interactive codenames game. You have two options: \n"
+          "1) Guess words using a clue generated by our sender \n"
+          "2) Choose a clue and give it to our receiver \n \n")
+    game_choice = rotem_utils.user_choice('1', "Which option do you choose?", options=['1', '2'], full_text=True)
+    print("now you choose the game parameters. To take the default, enter \'a\' at any decision. \n")
 
-    train_loader = CodenameDataLoader(tca, gca, embedding_method, vocab_method)
-    test_loader = CodenameDataLoader(tca, gca, embedding_method, vocab_method)
-    sender = TrainableSender(**kwargs)
-    vocab_size = sender.vocab_size
-    sender = core.GumbelSoftmaxWrapper(sender, temperature=1.0)
-    receiver = TrainableReceiver(**kwargs, input_size=rec_input_size)
-    receiver = core.SymbolReceiverWrapper(receiver, vocab_size, agent_input_size=rec_input_size)
+    tca = rotem_utils.user_choice(10, "total cards amount", demand_int=True)
+    gca = rotem_utils.user_choice(4, "good cards amount", demand_int=True)
+    vocab_method = rotem_utils.user_choice('nouns-kaggle', "vocabulary limitation",
+                                           options=['GloVe', 'word2vec', 'nltk-words', 'wordnet-nouns', 'nouns-kaggle'])
+    dist_metric = rotem_utils.user_choice('cosine_sim', "distance metric", options=['l2', 'cosine_sim'])
+    kwargs = dict(total_cards=tca, good_cards=gca, vocab_method=vocab_method, dist_metric=dist_metric)
 
-    game = core.SymbolGameGS(sender, receiver, rotem_utils.try_ce_loss)
-    optimizer = torch.optim.Adam(game.parameters())
-    trainer = core.Trainer(game=game, optimizer=optimizer, train_data=train_loader, validation_data=test_loader,
-                           callbacks=[core.TemperatureUpdater(agent=sender, decay=0.9, minimum=0.1)])
-    trainer.train(n_epochs=1)
+    if game_choice == '1':
+        sender_type = rotem_utils.user_choice('exhaustive', "sender type", options=['exhaustive', 'cluster'])
+        emb_method = rotem_utils.user_choice('GloVe', "sender embedding method", options=['GloVe', 'word2vec'])
+        dataloader = CodenameDataLoader(tca, gca, emb_method, emb_method, vocab_method)
+        data_generator = iter(dataloader)
+        sender_class = {'exhaustive': ExhaustiveSearchSender, 'cluster': EmbeddingClusterSender}[sender_type]
+        sender = sender_class(**kwargs, embedding_method=emb_method)
+
+        while True:
+            sender_input, receiver_input = next(data_generator)
+            print(f"words on the board: \n {receiver_input} \n")
+            clue, clue_num, targets = sender(sender_input, verbose=True)
+            print(f"The sender gives the clue \"{clue}\", for {clue_num} words. Can you guess them?")
+            guesses = []
+            for i in range(clue_num):
+                guess = rotem_utils.user_choice('stop', f"guess number {i+1}:", options=['stop'] + receiver_input,
+                                                full_text=True)
+                if guess == 'stop':
+                    break
+                guesses.append(guess)
+            guesses.sort()
+            print(f"your guesses: \t {guesses} \n you got right: \t {[w for w in guesses if w in targets]} \n"
+                  f"you got wrong: \t {[w for w in guesses if w not in targets]} \n the true words: \t {targets}")
+            answer = rotem_utils.user_choice('yes', "play again?", options=['yes', 'no'], full_text=True)
+            if answer == 'no':
+                break
+    else:   # game_choice == '2'
+        emb_method = rotem_utils.user_choice('GloVe', "receiver embedding method", options=['GloVe', 'word2vec'])
+        dataloader = CodenameDataLoader(tca, gca, emb_method, emb_method, vocab_method)
+        receiver = EmbeddingNearestReceiver(**kwargs, embedding_method=emb_method)
+        data_generator = iter(dataloader)
+        while True:
+            sender_input, receiver_input = next(data_generator)
+            print(f"\tgood words: \t {sender_input[0]}\n \tbad words: \t {sender_input[1]}")
+            print("What is your clue? \n")
+            while True:
+                clue = input()
+                if dataloader.legal_word(clue):
+                    break
+                print("illegal word. try again. \n")
+            clue_num = rotem_utils.user_choice(1, "number of guesses", demand_int=True)
+            assert clue_num <= tca, "too many guesses required"
+            choice = receiver(receiver_input, (clue, clue_num))
+            print(f"Our receiver guessed: \n \t {sorted(choice)} - "
+                  f"{100 * len([x for x in choice if x in sender_input[0]]) / clue_num}% correct.")
+            answer = rotem_utils.user_choice('yes', "play again?", options=['yes', 'no'], full_text=True)
+            if answer == 'no':
+                break
+
 
 if __name__ == '__main__':
-    skyline_main()
+    # skyline_main(tca=10, gca=5,
+    #              sender_type='exhaustive', sender_emb_method='GloVe', receiver_emb_method='word2vec',
+    #              vocab_method='nouns-kaggle', dist_metric='cosine_sim',
+    #              trial_amount=10, verbose=True)
+    interactive_game()
--- rotem_utils.py
+++ rotem_utils.py
@@ -1,21 +1,61 @@
-from abc import ABC
-
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
-
-import egg.core as core
-from transformers import BertTokenizer, BertModel
 
 import numpy as np
 import pickle
-import math
+import time
 import os
+from os.path import join, isdir, isfile
 from tqdm import tqdm
-from itertools import combinations
 import nltk
 from nltk.corpus import words, wordnet as wn
+
+import pandas as pd
+import gensim.downloader as api
+
+
+# global variable - name of embedding savings directory
+embedding_saves_dir = "embeddings_saved"
+glove_embeddings_file = "saved_glove_embeddings.pkl"
+word2vec_embeddings_file = "saved_w2v_embeddings.pkl"
+
+
+def load_saved_embedding(file_name):
+    if not isdir(embedding_saves_dir):  # no directory 'embeddings_saved'
+        os.mkdir(embedding_saves_dir)
+        return None
+
+    if not isfile(path := join(embedding_saves_dir, file_name)):    # no file file_name in the directory
+        return None
+
+    with open(path, "rb") as read_file:
+        x = pickle.load(read_file)
+    return x
+
+
+def dump_embeddings(file_name, obj):
+    assert isdir(embedding_saves_dir)
+    with open(join(embedding_saves_dir, file_name), "wb") as write_file:
+        pickle.dump(obj, write_file)
+
+
+def user_choice(def_val, text_param: str, options: list = None, demand_int: bool = False, full_text: bool = False):
+    assert demand_int or options
+    extra_text = 'choose an integer' if demand_int else 'out of ' + ' or '.join(map(lambda x: f"\'{x}\'", options))
+    while True:
+        inp = input(text_param if full_text else f"What is the {text_param}? (default: {def_val}) \n ({extra_text}) \n")
+        if inp == 'a':
+            return def_val
+        if demand_int:
+            if not inp.isdigit():
+                print("please choose an integer. \n")
+                continue
+            return int(inp)
+        if inp not in options:
+            print("this input is not an option. choose out of: \n", options)
+            continue
+        return inp
 
 
 def initiate_GloVe(save_data=True):
@@ -34,20 +74,37 @@ def initiate_GloVe(save_data=True):
     embeddings = torch.cat([embeddings, unk_vector], dim=0)
     glove_data = (embeddings, word_indexing_dict)
     if save_data:
-        with open("saved_glove_embeddings.pkl", "wb") as write_file:
-            pickle.dump(glove_data, write_file)
+        dump_embeddings(glove_embeddings_file, glove_data)
     return glove_data
+
+
+def initiate_word2vec(save_data=True):
+    gensim_model = api.load('word2vec-google-news-300')
+    word_indexing_dict = gensim_model.key_to_index
+    embeddings = torch.from_numpy(gensim_model.vectors)
+    unk_vector = embeddings.mean(dim=0, keepdim=True)
+    embeddings = torch.cat([embeddings, unk_vector], dim=0)
+    w2v_data = (embeddings, word_indexing_dict)
+    if save_data:
+        dump_embeddings(word2vec_embeddings_file, w2v_data)
+    return w2v_data
 
 
 def create_embedder(method: str):
     if method == 'GloVe':
-        if os.path.isfile("saved_glove_embeddings.pkl"):
-            with open("saved_glove_embeddings.pkl", "rb") as read_file:
-                embeddings, word_indexing_dict = pickle.load(read_file)
-        else:
+        x = load_saved_embedding(glove_embeddings_file)
+        if x is None:
             embeddings, word_indexing_dict = initiate_GloVe()
+        else:
+            embeddings, word_indexing_dict = x
+    elif method == 'word2vec':
+        x = load_saved_embedding(word2vec_embeddings_file)
+        if x is None:
+            embeddings, word_indexing_dict = initiate_word2vec()
+        else:
+            embeddings, word_indexing_dict = x
     else:
-        raise ValueError("currently, only GloVe embedding is allowed")
+        raise ValueError("currently, only GloVe and word2vec embedding are allowed")
 
     index_word_dict = {v:k for k,v in word_indexing_dict.items()}
 
@@ -71,31 +128,60 @@ def get_vocabulary(method: str):
             x = wn.all_synsets('n')
         return list(map(lambda s: s.name().partition('.')[0], x))
 
+    if method == 'nouns-kaggle':
+        # taken from https://www.kaggle.com/datasets/leite0407/list-of-nouns
+        return ['atm'] + np.char.lower(pd.read_csv('nounlist.csv').to_numpy().reshape(-1).astype(str)).tolist()
+
     _a, word_indexing_dict, _b = create_embedder(method)
     return list(word_indexing_dict.keys())
 
 
 def get_embedder_with_vocab(embed_method: str = 'GloVe', vocab_method: str = 'wordnet-nouns'):
-    embeddings, word_indexing_dict, index_word_dict = create_embedder(embed_method)
-    all_vocab = get_vocabulary(vocab_method)
-    vocab = [v for v in all_vocab if v in word_indexing_dict]
-    print(f"{100 * len(vocab) / len(all_vocab)}% of the vocabulary \'{vocab_method}\' can be used with"
-          f" \'{embed_method}\'embedding. Total of {len(vocab)} words")
-    vocab_indices = [word_indexing_dict[word] for word in vocab]
-    embeddings = embeddings[vocab_indices + [-1]]
-    word_indexing_dict = {v:i for i,v in enumerate(vocab)}
-    index_word_dict = {i: v for i, v in enumerate(vocab)}
+    filename = f"embed_{embed_method}_vocab_{vocab_method}.pkl"
+    x = load_saved_embedding(filename)
+    if x is not None:
+        embeddings, word_indexing_dict, index_word_dict, vocab = x
+    else:
+        embeddings, word_indexing_dict, _ = create_embedder(embed_method)
+        all_vocab = get_vocabulary(vocab_method)
+        vocab = [v for v in all_vocab if v in word_indexing_dict]
+        print(f"{100 * len(vocab) / len(all_vocab)}% of the vocabulary \'{vocab_method}\' can be used with"
+              f" \'{embed_method}\'embedding. Total of {len(vocab)} words")
+        vocab_indices = [word_indexing_dict[word] for word in vocab]
+        embeddings = embeddings[vocab_indices + [-1]]
+        word_indexing_dict = {v:i for i,v in enumerate(vocab)}
+        index_word_dict = {i: v for i, v in enumerate(vocab)}
+
+        dump_embeddings(filename, (embeddings, word_indexing_dict, index_word_dict, vocab))
     return embeddings, word_indexing_dict, index_word_dict, vocab
+
+
+def triple_restricted_vocab(embed_method_1: str, embed_method_2: str, vocab_method: str):
+    """ restrict the vocabulary to all three methods """
+    t0 = time.perf_counter()
+    *_, v1 = get_embedder_with_vocab(embed_method_1, vocab_method)
+    if embed_method_1 == embed_method_2:
+        return v1
+    *_, v2 = get_embedder_with_vocab(embed_method_2, vocab_method)
+    result = list(set(v1).intersection(set(v2)))
+    print("time for triple_restriced_vocab:", time.perf_counter() - t0)
+    return result
 
 
 class EmbeddingAgent(nn.Module):
     def __init__(self, total_cards_amount: int, good_cards_amount: int, embedding_method: str = 'GloVe',
-                 vocab_method: str = 'wordnet-nouns'):
+                 vocab_method: str = 'wordnet-nouns', dist_metric: str = 'l2'):
         super(EmbeddingAgent, self).__init__()
         self.gca = good_cards_amount
         self.tca = total_cards_amount
         self.embeddings, self.word_index_dict, self.index_word_dict, _ = get_embedder_with_vocab(embedding_method,
                                                                                                  vocab_method)
+        assert dist_metric in ['cosine_sim', 'l2']
+        if dist_metric == 'cosine_sim':
+            # Normalize the embedding vectors. Euclidean norm is now rank-equivalent to cosine similarity, so no
+            # further change is needed.
+            embedding_norms = torch.norm(self.embeddings, dim=-1, keepdim=True)
+            self.embeddings = self.embeddings / embedding_norms
 
     def embedder(self, word: str):
         return self.embeddings[self.word_index_dict.get(word, -1)]
@@ -107,3 +193,50 @@ def try_ce_loss(sender_input, _message, _receiver_input, receiver_output, _label
 
 #########################
 
+# def main():
+#     gca = 4
+#     tca = 10
+#     embedding_method = 'GloVe'
+#     vocab_method = 'wordnet-nouns'
+#     rec_input_size = 400
+#     kwargs = dict(total_cards=tca, good_cards=gca, embedding_method=embedding_method, vocab_method=vocab_method)
+#     opts = core.init(params=['--random_seed=7',  # will initialize numpy, torch, and python RNGs
+#                              '--lr=1e-3',  # sets the learning rate for the selected optimizer
+#                              '--batch_size=32',
+#                              '--optimizer=adam'])
+#
+#     train_loader = CodenameDataLoader(tca, gca, embedding_method, vocab_method)
+#     test_loader = CodenameDataLoader(tca, gca, embedding_method, vocab_method)
+#     sender = TrainableSender(**kwargs)
+#     vocab_size = sender.vocab_size
+#     sender = core.GumbelSoftmaxWrapper(sender, temperature=1.0)
+#     receiver = TrainableReceiver(**kwargs, input_size=rec_input_size)
+#     receiver = core.SymbolReceiverWrapper(receiver, vocab_size, agent_input_size=rec_input_size)
+#
+#     game = core.SymbolGameGS(sender, receiver, rotem_utils.try_ce_loss)
+#     optimizer = torch.optim.Adam(game.parameters())
+#     trainer = core.Trainer(game=game, optimizer=optimizer, train_data=train_loader, validation_data=test_loader,
+#                            callbacks=[core.TemperatureUpdater(agent=sender, decay=0.9, minimum=0.1)])
+#     trainer.train(n_epochs=1)
+
+
+# class TrainableSender(rotem_utils.EmbeddingAgent):
+#     def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str):
+#         super(TrainableSender, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
+#         self.vocab_size = len(self.word_index_dict)
+#         self.fc = nn.Linear(100, self.vocab_size)
+#
+#     def forward(self, x, aux_input=None):
+#         print(f"sender inputs: \n \t x: {x} \n \t aux: {aux_input} \n \n")
+#         return None
+#
+#
+# class TrainableReceiver(rotem_utils.EmbeddingAgent):
+#     def __init__(self, total_cards: int, good_cards: int, embedding_method: str, vocab_method: str, input_size: int):
+#         super(TrainableReceiver, self).__init__(total_cards, good_cards, embedding_method, vocab_method)
+#         self.fc = nn.Linear(input_size, 10)     # 10 tbd
+#
+#     def forward(self, channel_input, receiver_input, aux_input=None):
+#         print(f"Receiver inputs: \n \t channel_input: {channel_input} \n \t receiver_input: {receiver_input}"
+#               f" \n \t aux: {aux_input} \n \n")
+#         return None
